388ce1ddfb0417851115412d766cb8c72ff3991b
Jesse Halley <halley.jesse@me.com>
2021-04-23 22:11:02 +1000

    Add wet/dry metric tonne units

---
+++ Sources/ConradEntryReport/Extensions/Unit+Report.swift
@@ -0,0 +1,19 @@
+//
+// ConradEntryReport
+// Copyright © 2021 Jesse Halley. All rights reserved.
+//
+
+import Foundation
+
+public extension UnitMass {
+    static let wetMetricTonnes = UnitMass(symbol: "WMT", converter: UnitConverterLinear(coefficient: 1_000))
+    static let dryMetricTonnes = UnitMass(symbol: "DMT", converter: UnitConverterLinear(coefficient: 1_000))
+}
+
+public final class UnitDischargeRate: Dimension {
+    public static let wetMetricTonnesPerHour = UnitDischargeRate(symbol: "WMT/hr", converter: UnitConverterLinear(coefficient: 1))
+    public static let dryMetricTonnesPerHour = UnitDischargeRate(symbol: "DMT/hr", converter: UnitConverterLinear(coefficient: 1))
+
+    public static let wetMetricTonnesPerDay = UnitDischargeRate(symbol: "WMT/day", converter: UnitConverterLinear(coefficient: 24))
+    public static let dryMetricTonnesPerDay = UnitDischargeRate(symbol: "DMT/day", converter: UnitConverterLinear(coefficient: 24))
+}
