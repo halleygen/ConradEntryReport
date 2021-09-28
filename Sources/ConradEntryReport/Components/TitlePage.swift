f531283cb403f640e463b38410423406d7915a01
Jesse Halley <halley.jesse@me.com>
2021-09-28 10:53:37 +1000

    Allow for optional values to be passed to TitlePage components (nil values are not rendered)

--- Sources/ConradEntryReport/Components/TitlePage.swift
+++ Sources/ConradEntryReport/Components/TitlePage.swift
@@ -9,11 +9,11 @@ import Plot
 public struct TitlePage: Component {
     public let title: String
     public let vesselPhoto: Figure
-    public let components: KeyValuePairs<String, HTMLTextConvertible>
+    public let components: KeyValuePairs<String, HTMLTextConvertible?>
 
     @EnvironmentValue(.context) private var context
 
-    public init(title: String, vesselPhoto: Figure, components: KeyValuePairs<String, HTMLTextConvertible>) {
+    public init(title: String, vesselPhoto: Figure, components: KeyValuePairs<String, HTMLTextConvertible?>) {
         self.title = title
         self.vesselPhoto = vesselPhoto
         self.components = components
@@ -24,8 +24,10 @@ public struct TitlePage: Component {
             H1(title)
 
             for (name, value) in components {
-                Text("\(name): \(value.htmlString(context: context!))")
-                    .bold()
+                if let value = value {
+                    Text("\(name): \(value.htmlString(context: context!))")
+                        .bold()
+                }
             }
 
             vesselPhoto
